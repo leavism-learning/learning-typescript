90202510303bdcdc85a3ab968b0c76e8687b8ae0
Huy Dang <ghuydang@gmail.com>
2023-01-05 14:12:33 -0700

    Structural typing
    It's also called duck typing. This terms comes from the saying "If it walks like a duck and quacks like a duck, then it must be a duck." Pretty funny I guess.

--- types.ts
+++ types.ts
@@ -6,3 +6,15 @@ interface Bag<T> {
 }
 
 declare const sack: Bag<string>;
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+function logPoint(p: Point) {
+  console.log(`(${p.x}, ${p.y})`);
+}
+
+const coordinate = { x: 10, y: 12 };
+logPoint(coordinate);
