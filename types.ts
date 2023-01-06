26181f3259a526f7ae825ed1b1852b8002d0fd41
Huy Dang <ghuydang@gmail.com>
2023-01-06 11:18:00 -0700

    Generics with type variable
    An alternative to taking args and returns of type any. This keeps them consistent.

--- types.ts
+++ types.ts
@@ -30,4 +30,19 @@ class VirtualPoint {
 }
 
 const newVPoint = new VirtualPoint(13, 56);
-logPoint(newVPoint); // logs "13, 56"
+logPoint(newVPoint);
+
+// Generics in functions. Captures the Type inside < > and can use it in the parameter and return
+// Whatever Type is, it stays consistent.
+// In this case, the arg type has the stay the same as the return type.
+function identify<Type>(arg: Type): Type {
+  // do something
+  return arg;
+}
+
+// Using Type is different from any because the type stays consistent with Type.
+// In this case, the parameter arg could take in a number, but then the output could be a string
+function anyIdentify(arg: any): any {
+  // do something
+  return arg;
+}
