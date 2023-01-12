208b1e114b167cad0e4a42dff183219ce0fe1df6
Huy Dang <ghuydang@gmail.com>
2023-01-12 12:20:42 -0700

    Typeof

--- typeof.ts
+++ typeof.ts
@@ -1,2 +1,9 @@
 type Predicate = (x: unknown) => boolean;
 type K = ReturnType<Predicate>;
+
+function func() {
+  return { x: 10, y: 3 };
+}
+
+// type P = ReturnType<func>; >> Error because values and types aren't the same thing
+type P = ReturnType<typeof func>; // Have to refure the the return type, not the return value
