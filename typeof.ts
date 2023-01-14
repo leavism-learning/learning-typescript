1f0e61ba38581f955c51354a8999779740e4098c
Huy Dang <ghuydang@gmail.com>
2023-01-13 23:48:33 -0700

    Indexed access types

--- typeof.ts
+++ typeof.ts
@@ -7,3 +7,14 @@ function func() {
 
 // type P = ReturnType<func>; >> Error because values and types aren't the same thing
 type P = ReturnType<typeof func>; // Have to refure the the return type, not the return value
+
+// use an indexed access type to look up
+// specific property on another type
+type Person = { age: number; name: string; alive: boolean };
+type Age = Person['age'];
+
+type I1 = Person['age' | 'name'];
+type I2 = Person[keyof Person];
+
+type AliveOrName = 'alive' | 'name';
+type I3 = Person[AliveOrName];
