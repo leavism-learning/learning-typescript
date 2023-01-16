8c1903e6f65bca2cf9c7430390aea0b0078e6c84
Huy Dang <ghuydang@gmail.com>
2023-01-16 10:36:25 -0700

    Conditioinal types with generic functions
    They're pretty powerful as a pair!

--- conditional_types.ts
+++ conditional_types.ts
@@ -7,3 +7,29 @@ interface Dog extends Animal {
 
 type Example1 = Dog extends Animal ? number : string;
 type Example2 = RegExp extends Animal ? number : string;
+
+// Even better with generics
+// Look at this example, which has a bunch of function overloads
+// Not a good idea
+interface IdLabel {
+  id: number /* some fields */;
+}
+interface NameLabel {
+  name: string /* other fields */;
+}
+
+function createLabel(id: number): IdLabel;
+function createLabel(name: string): NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel {
+  throw 'unimplemented';
+}
+
+// this is where generics come in with conditional types
+type NameOrId<T extends number | string> = T extends number
+  ? IdLabel
+  : NameLabel;
+
+function createLabel<T extends number | string>(idOrName: T): NameOrId<T> {
+  throw 'unimplemented';
+}
