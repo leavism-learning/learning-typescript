6a8a1b5f4c4dc27834dd74b2e1802bbc99fb0acc
Huy Dang <ghuydang@gmail.com>
2023-01-26 12:57:48 -0700

    Removes any optional properties

--- mapped_types.ts
+++ mapped_types.ts
@@ -20,3 +20,16 @@ type LockedAccount = {
 };
 
 type UnlockedAccount = CreateMutable<LockedAccount>;
+
+// Removes 'optional' attributes from a type's properties
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+
+type User = Concrete<MaybeUser>;
