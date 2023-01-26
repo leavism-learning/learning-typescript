006c2246ec6fdd40448784df4f4567c5cf1c1ffb
Huy Dang <ghuydang@gmail.com>
2023-01-26 12:07:22 -0700

    readonly modifier

--- mapped_types.ts
+++ mapped_types.ts
@@ -9,3 +9,14 @@ const conforms: OnlyBoolsAndHorses = {
   del: true,
   rodney: false,
 };
+
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+};
+
+type UnlockedAccount = CreateMutable<LockedAccount>;
