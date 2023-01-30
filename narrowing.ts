f04456a8fe9e8c5d04b63d4561232fc85c1c610e
Huy Dang <ghuydang@gmail.com>
2023-01-30 15:03:35 -0700

    Narrowing with type guards

---
+++ narrowing.ts
@@ -0,0 +1,5 @@
+// Type guarding
+function padLeft(padding: number | string, input: string): string {
+  if (typeof padding === 'number') return ' '.repeat(padding) + input;
+  return padding + input;
+}
